8f4f09b881b35595e8eb8ad367c24f263660d8cb
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 12:28:49 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = props => (
         <p>
                    referral only concierge medicine by{' '}
           <a href="https://kottonmd.com">Dr Ryan H. Kotton MD, personalized to you. 
-           <br />born in Los Angeles</a>
+           <br>born in Los Angeles</a>
         </p>
          
       </div>
