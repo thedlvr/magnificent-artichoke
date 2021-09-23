aaa17c6635bc0ac2a2c101847581b2fec5d7bbb7
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 12:40:00 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">&copy; Bespoke Health - Los Angeles | Aspen | San Francisco | 
-         New York |  <a href=""></a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+         New York |  <a href=""></a>. Built with: <a href="https://www.gatsbyjs.org/">More</a></p>
     </footer>
 )
 
