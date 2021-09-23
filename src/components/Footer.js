73cef91fca30496c63dbbb6608fda379584865f8
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 12:35:51 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; bespoe Health - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">&copy; Bespoe Health - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
 
