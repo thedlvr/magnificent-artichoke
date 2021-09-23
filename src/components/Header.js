0ad703534057e522af3ea0f54cefe33c64e1f91a
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 12:23:45 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = props => (
         <h1>Dimension</h1>
         <p>
                    referral only concierge medicine by{' '}
-          <a href="https://kottonmd.com">Dr Ryan H. Kotton MD, personalized to you.</a>
+          <a href="https://kottonmd.com">Dr Ryan H. Kotton MD, personalized to you. <br /></a>
         </p>
          <p>
                    referral only concierge medicine by{' '}
