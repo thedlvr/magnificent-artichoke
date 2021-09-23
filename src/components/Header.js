363ad85e53bf657a4ce2f59211df3fa94a4952d6
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 12:19:20 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = props => (
         <h1>Dimension</h1>
         <p>
                    referral only concierge medicine by{' '}
-          <a href="https://kottonmd.com">Dr Ryan H. Kotton MD, bespoke to you</a>
+          <a href="https://kottonmd.com">Dr Ryan H. Kotton MD, tailored to you</a>
           <br />
           <a href=""></a> license.
         </p>
