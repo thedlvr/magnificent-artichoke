3255299c58e709b17d90de56815f01e76d4a6fe8
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 12:12:38 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = props => (
         <h1>Dimension</h1>
         <p>
           invitation only concierge medicine{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          <a href="https://kottonmd.com">HTML5 UP</a> and released
           <br />
           for free under the{' '}
           <a href="https://html5up.net/license">Creative Commons</a> license.
